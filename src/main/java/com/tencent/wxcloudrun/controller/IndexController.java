5dcf9097f64104f19a3677007cb48f8e28d6f63d
liyuyun <liyuyun0121@163.com>
2023-04-13 22:24:59 +0800

    fix: update Request to Map

--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -6,10 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -28,10 +31,29 @@ public class IndexController {
     return "index";
   }
 
+  /**
+   * {ToUserName=gh_c0dcfafd5ba7, FromUserName=ocfKe5yo_AnWkd8kdcorTv7SXlfU, CreateTime=1681394782, MsgType=text, Content=2, MsgId=24071788176428433}
+   * @param map
+   * @return
+   * @throws IOException
+   */
   @PostMapping
-  public void chat(@RequestBody Map<String, Object> map, HttpServletResponse response) throws IOException {
+  @ResponseBody
+  public Map<String, Object> chat(@RequestBody Map<String, Object> map) throws IOException {
     System.out.println(map);
-    response.getWriter().write("it's response, get map: " + map);
+    String toUserName = (String) map.get("ToUserName");
+    String fromUserName = (String) map.get("FromUserName");
+    Long createTime = System.currentTimeMillis();
+    String msgType = "text";
+    String content = (String) map.get("Content");
+    Map<String, Object> responseMap = new HashMap<>();
+    responseMap.put("ToUserName", toUserName);
+    responseMap.put("FromUserName", fromUserName);
+    responseMap.put("CreateTime", createTime);
+    responseMap.put("MsgType", msgType);
+    responseMap.put("Content", content);
+    System.out.println("responseMap: " + responseMap);
+    return responseMap;
   }
 
 }
