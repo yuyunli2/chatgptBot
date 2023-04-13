022d3685ceec50aacdcd54d8f243b8b999580fe9
liyuyun <liyuyun0121@163.com>
2023-04-13 21:29:34 +0800

    feat: add chat method

--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -1,7 +1,14 @@
 package com.tencent.wxcloudrun.controller;
 
+import com.tencent.wxcloudrun.dto.ChatRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * index控制器
@@ -17,6 +24,13 @@ public class IndexController {
   @GetMapping
   public String index() {
     return "index";
+  }
+
+  @PostMapping
+  public void chat(@RequestBody ChatRequest request, HttpServletResponse response) throws IOException {
+    String content = request.getContent();
+    System.out.println("content: " + content);
+    response.getWriter().write("it's response, get conent: " + content);
   }
 
 }
